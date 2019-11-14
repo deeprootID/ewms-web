df8d7fac6a9d4a20d8861f3e9123a7743c579642
unknown <revinaawalia@gmail.com>
2019-11-15 06:56:11 +0700

    Initial Commit

---
+++ src/views/Warehouses.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="animated fadeIn">
+    <br>
+    <b-row align="center" align-h="center">
+      <b-col sm="6" md="4">
+        <b-card header="Warehouse 1" header-bg-variant="primary">
+          <b-row>
+            <b-col>
+              <p>fjdkafjkafak</p>
+            </b-col>
+            <b-col>
+              <p>fjdkafjkafak</p>
+            </b-col>
+          </b-row>
+          <b-row>
+              <b-col>
+                  <ul style="list-style-type:none;">
+                    <li>aaaaa</li>
+                    <li>aaaaa</li>
+                    <li>aaaaa</li>
+                  </ul>
+              </b-col>
+              <b-col>
+                  <ul style="list-style-type:none;">
+                    <li>aaaaa</li>
+                    <li>aaaaa</li>
+                    <li>aaaaa</li>
+                  </ul>
+              </b-col>
+          </b-row>
+        </b-card>
+      </b-col>
+    </b-row>
+  </div>
+</template>
